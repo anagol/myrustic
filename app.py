6d0f3c3d6e176f49863f9aedbe065ce22eb4142a
anagol <golosnoy69@gmail.com>
2020-10-07 18:01:42 +0300

    PC

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-from flask import Flask, redirect, render_template, url_for, request
-
+from flask import Flask, redirect, render_template, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'anatolihalasny1969'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-class User(db.Model):
+# -----------------------Создаем базу данных-----------------------------------------------------------------
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(250))
 
-    def __init__(self, username, email, password):
+    def __init__(self, username, password):
         self.username = username
-        self.email = email
         self.password = password
 
     def __repr__(self):
@@ -36,8 +42,7 @@ class Product(db.Model):
     product_term = db.Column(db.String(200), unique=False)
 
     def __init__(self, product_code, product_name, product_photo, product_description, product_price, product_size,
-                 product_weight,
-                 product_material, product_term):
+                 product_weight, product_material, product_term):
         self.product_code = product_code
         self.product_name = product_name
         self.product_photo = product_photo
@@ -52,24 +57,42 @@ class Product(db.Model):
         return '<Product %r>' % self.product_name
 
 
+class Guest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pub_date = db.Column(db.DateTime(timezone=True), nullable=False, default=func.current_timestamp())
+    name = db.Column(db.String(80))
+    message = db.Column(db.Text)
+
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
+
+    def __repr__(self):
+        return '<Name %r >' % self.name
+
+
+# ------------------------Главная страница-------------------------------------------------------------------
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Главная страница')
 
 
+# -----------------------Создаем общую страницу с изделиями--------------------------------------------------
 @app.route('/product')
 def product():
     product = Product.query.filter_by().all()
     return render_template('product.html', title='Мои изделия', product=product)
 
 
+# -----------------------Создаем общую страницу каждого изделиями--------------------------------------------
 @app.route('/<int:prod_id>')
 def prod(prod_id):
     prod = Product.query.filter_by(id=prod_id).one()
     return render_template('prod.html', prod=prod)
 
 
+# -----------------------Страница создания каждого изделия---------------------------------------------------
 @app.route('/create_product', methods=['GET', 'POST'])
 def create_product():
     if request.method == "POST":
@@ -92,17 +115,14 @@ def create_product():
     return render_template('create_product.html', title='Добавляем изделие')
 
 
+# -----------------------Страница редактирования изделий-----------------------------------------------------
 @app.route('/edit_product')
 def edit_product():
     product = Product.query.all()
     return render_template('edit_product.html', title='Редактируем', product=product)
 
 
-# @app.route('/edit')
-# def edit():
-#     return render_template('edit.html', title='Редактируем изделия')
-
-
+# -----------------------Страница редактирования каждого изделия---------------------------------------------
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
     prod = Product.query.get_or_404(id)
@@ -123,6 +143,7 @@ def edit(id):
         return render_template('edit.html', prod=prod)
 
 
+# -----------------------Функционал удаления отдельного изделия----------------------------------------------
 @app.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
     verse = Product.query.get_or_404(id)
@@ -132,19 +153,101 @@ def delete(id):
     return redirect(url_for('product'))
 
 
+# -----------------------Страница "Обо мне"------------------------------------------------------------------
 @app.route('/about')
 def about():
     return render_template('about.html', title='Обо мне')
 
 
+# -----------------------Страница "Доставка"-----------------------------------------------------------------
 @app.route('/delivery')
 def delivery():
     return render_template('delivery.html', title='Доставка')
 
 
+# -----------------------Страница "Оплата"-------------------------------------------------------------------
 @app.route('/payment')
 def payment():
     return render_template('payment.html', title='Оплата')
+
+
+# -----------------------Страница "Отзывы"-------------------------------------------------------------------
+@app.route('/guest', methods=['GET', 'POST'])
+def guest():
+    guest = Guest.query.all()
+    if request.method == "POST":
+        name = request.form["name"]
+        message = request.form["message"]
+        guest = Guest(name=name, message=message)
+        db.session.add(guest)
+        db.session.flush()
+        db.session.commit()
+        return redirect(url_for('guest'))
+    return render_template('guest.html', title='Гостевая книга', guest=guest)
+
+
+# -----------------------Страница редактирования отзывов-----------------------------------------------------
+@app.route('/guest_edit')
+def guest_edit():
+    guest = Guest.query.all()
+    return render_template('guest_edit.html', title='Редактируем гостевую книгу', guest=guest)
+
+
+@app.route('/<int:id>/guest_delete', methods=('POST',))
+def guest_delete(id):
+    guest = Guest.query.get_or_404(id)
+    db.session.delete(guest)
+    db.session.flush()
+    db.session.commit()
+    return redirect(url_for('guest_edit'))
+
+
+# -----------------------Страница администратора-------------------------------------------------------------
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', title='Страница администратора')
+
+
+# --------------------Регистрация---------------------------------------------------------------------------
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password_hash = generate_password_hash(request.form["password"])
+        register = User(username=username, password=password_hash)
+        db.session.add(register)
+        db.session.flush()
+        db.session.commit()
+        flash('Вы успешно зарегистрированы, теперь можете войти в систему!')
+        return redirect(url_for("login"))
+    return render_template("register.html", title='Регистрация')
+
+
+#  --------------------Login-------------------------------------------------------------------------------------------
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        user = User.query.filter_by(username=username).first()
+        login_user(user)
+        if user is None:
+            flash('Неверная пара логин - пароль')
+            return render_template("login.html")
+        if not check_password_hash(user.password, request.form['password']):
+            flash('Неверная пара логин - пароль')
+            return render_template("login.html")
+        flash(f'Вы успешно авторизованы под именем {username}!')
+        return redirect(url_for("admin"))
+
+    return render_template("login.html")
+
+
+#  --------------------Logout------------------------------------------------------------------------------------------
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
